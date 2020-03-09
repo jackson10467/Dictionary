98116bd5e0b686aba5a70a86c2cc5783149b6b9e
Jackson Vu <jacksonvu@Jacksons-MacBook-Pro.local>
2020-03-09 10:41:57 -0400

    search function code added

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const apiKey = "1213f19772msha8236f6e8356a42p1f62d3jsndb481029ea07"
+const apiKey = "1213f19772msha8236f6e8356a42p1f62d3jsndb481029ea07";
 
-let word = document.querySelector(".word");
+let div = document.querySelector(".word");
 const button = document.querySelector(".button");
+
 
 button.addEventListener("click", async () => {
   const response = await axios({
@@ -19,6 +20,19 @@ button.addEventListener("click", async () => {
   })
     // how would i translate this back to the simple "axios.get(URL, {header}"?
     console.log(response);
-})//RapidAPI gave me this code block ) = 
+})//RapidAPI gave me this code block ) =
 
-
+// button.addEventListener("click", async () => {
+//   const input = document.querySelector("input").value;
+//   const response = await axios({
+//     "method" : "GET",
+//     "url": `"https://wordsapiv1.p.rapidapi.com/words/${input}/definitions"`,
+//     "headers": {
+//       "content-type": "application/octet-stream",
+//       "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+//       "x-rapidapi-key": "1213f19772msha8236f6e8356a42p1f62d3jsndb481029ea07"
+//     }
+//   });
+//   console.log(input);
+//   console.log(response);
+// });
